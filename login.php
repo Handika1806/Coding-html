c66e524a90b3f6960c0a29ee7eba8844a19c57b4
Hacking Express Grounds <101469168+Handika1806@users.noreply.github.com>
2023-10-05 14:58:48 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,20 @@
+    <form method="POST" action="../index.php">
+
+                <!-- tipe hidden tidak akan tampil pada website --> 
+                <input name="tujuan" type="hidden" value="LOGIN" >
+
+                <label>Username</label>
+                <br>
+                <input name="username" type="text">
+                <br>
+                <label>Password</label>
+                <br>
+                <input name="password" type="password">
+                <br>
+
+                <button>Log In</button>
+                
+                <p> Belum punya akun?
+                  <a href="register.php">Daftar di sini</a>
+                </p>
+            </form>
---
+++ register.html
@@ -0,0 +1,19 @@
+    <form method="POST" action="../index.php">
+      
+                <input name="tujuan" type="hidden" value="LOGIN">
+
+                <label>Username</label>
+                <br>
+                <input name="username" type="text">
+                <br>
+                <label>Password</label>
+                <br>
+                <input name="password" type="password">
+                <br>
+
+                <button>Register</button>
+                
+                <p> Belum punya akun?
+                  <a href="login.php">Daftar di sini</a>
+                </p>
+            </form>
