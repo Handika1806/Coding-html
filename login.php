802a897a8ac440c2ad8816fca1ee0fdbcd845e1f
Hacking Express Grounds <101469168+Handika1806@users.noreply.github.com>
2023-10-22 12:57:16 +0700

    Create login.php

---
+++ login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="id" encoding="utf-8">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Form Register</title>
+</head>
+<body>
+    <form action="tersambung.php" method="post">
+        <label for="Username">Username</label>
+        <input type="text" name="username" autocomplete="off" />
+        <br>
+        <label for="Email">Email</label>
+        <input type="text" name="email" autocomplete="off" />
+        <br>
+        <label for="Umur">Umur</label>
+        <input type="text" name="umur" autocomplete="off" />
+        <br>
+         <button type="submit">submit</button>
+    </form>
+</body>
+</html>
